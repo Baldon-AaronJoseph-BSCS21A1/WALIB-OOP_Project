36c4a06f3da118a5d6a7df7fb5d7d29aece4cf90
Baldon-AaronJoseph-BSCS21A1 <aaronjosephtbaldon062505@gmail.com>
2024-11-03 00:08:45 +0800

    Add files via upload

---
+++ CodeSense - Statement Detector/Winproto/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Winproto
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI Black", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(281, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(200, 50);
+            label1.TabIndex = 0;
+            label1.Text = "CodeSense";
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Benguiat", 13.2000008F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Location = new Point(207, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(389, 145);
+            label2.TabIndex = 1;
+            label2.Text = "Your Programming Detective";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = Properties.Resources._1022330;
+            pictureBox1.Location = new Point(302, 96);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(149, 225);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Font = new Font("Cooper Std Black", 10.7999992F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(281, 327);
+            button1.Name = "button1";
+            button1.Size = new Size(189, 62);
+            button1.TabIndex = 3;
+            button1.Text = "Get Started";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox2.Image = Properties.Resources._101_1010012_download_png;
+            pictureBox2.InitialImage = (Image)resources.GetObject("pictureBox2.InitialImage");
+            pictureBox2.Location = new Point(620, 108);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(81, 111);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 4;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox3.Image = Properties.Resources.pythonlogo;
+            pictureBox3.Location = new Point(550, 239);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(110, 114);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 5;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.Transparent;
+            pictureBox4.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox4.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(67, 123);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(109, 127);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 6;
+            pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+    }
+}
---
+++ CodeSense - Statement Detector/Winproto/Form2.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Winproto
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            label1 = new Label();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            label3 = new Label();
+            button1 = new Button();
+            pictureBox2 = new PictureBox();
+            label4 = new Label();
+            button2 = new Button();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            label5 = new Label();
+            button3 = new Button();
+            label6 = new Label();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            label1.Font = new Font("Verdana", 18F, FontStyle.Bold | FontStyle.Italic);
+            label1.Location = new Point(128, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(540, 49);
+            label1.TabIndex = 0;
+            label1.Text = "PROGRAMMING LANGUAGES";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Benguiat", 13F, FontStyle.Bold);
+            label2.Location = new Point(201, 83);
+            label2.Name = "label2";
+            label2.Size = new Size(373, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Select a Programming Language";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox1.Image = Properties.Resources.d5w6c1f_755e9d03_d8b2_48d3_a1b6_3723cad6eaea_removebg_preview;
+            pictureBox1.Location = new Point(56, 150);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(138, 123);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BorderStyle = BorderStyle.Fixed3D;
+            label3.Font = new Font("Hobo Std", 13.2000008F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(87, 276);
+            label3.Name = "label3";
+            label3.Size = new Size(65, 35);
+            label3.TabIndex = 3;
+            label3.Text = "Java";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(70, 314);
+            button1.Name = "button1";
+            button1.Size = new Size(107, 34);
+            button1.TabIndex = 4;
+            button1.Text = "Select";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(233, 150);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(138, 123);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BorderStyle = BorderStyle.Fixed3D;
+            label4.Font = new Font("Hobo Std", 13.2000008F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(256, 276);
+            label4.Name = "label4";
+            label4.Size = new Size(92, 35);
+            label4.TabIndex = 6;
+            label4.Text = "Python";
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveCaption;
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(247, 314);
+            button2.Name = "button2";
+            button2.Size = new Size(115, 34);
+            button2.TabIndex = 7;
+            button2.Text = "Select";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(405, 150);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(138, 123);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 8;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(596, 150);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(138, 123);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 9;
+            pictureBox4.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BorderStyle = BorderStyle.Fixed3D;
+            label5.Font = new Font("Hobo Std", 13.2000008F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(448, 276);
+            label5.Name = "label5";
+            label5.Size = new Size(47, 35);
+            label5.TabIndex = 10;
+            label5.Text = "C#";
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveCaption;
+            button3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(417, 314);
+            button3.Name = "button3";
+            button3.Size = new Size(115, 34);
+            button3.TabIndex = 11;
+            button3.Text = "Select";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BorderStyle = BorderStyle.Fixed3D;
+            label6.Font = new Font("Hobo Std", 13.2000008F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(631, 276);
+            label6.Name = "label6";
+            label6.Size = new Size(60, 35);
+            label6.TabIndex = 12;
+            label6.Text = "C++";
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ActiveCaption;
+            button4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.Location = new Point(606, 314);
+            button4.Name = "button4";
+            button4.Size = new Size(115, 34);
+            button4.TabIndex = 13;
+            button4.Text = "Select";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(label6);
+            Controls.Add(button3);
+            Controls.Add(label5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(button2);
+            Controls.Add(label4);
+            Controls.Add(pictureBox2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Label label3;
+        private Button button1;
+        private PictureBox pictureBox2;
+        private Label label4;
+        private Button button2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private Label label5;
+        private Button button3;
+        private Label label6;
+        private Button button4;
+    }
+}
